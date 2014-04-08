0316c48654f88985ae42ee7d7aad9e47055aefaf
armandooj <armando.o.j@gmail.com>
2014-04-08 18:20:48 -0500

    Added basic evaluation for numeric operators

--- parsing.c
+++ parsing.c
@@ -9,7 +9,7 @@
 
 static char buffer[2048];
 
-char* readline (char* prompt) {
+char* readline(char* prompt) {
 	fputs("mexlisp> ", stdout);
 	fgets(buffer, 2048, stdin);
 	char *cpy = malloc(strlen(buffer) + 1);
@@ -28,6 +28,36 @@ void add_history(char* unused) {}
 #endif
 
 static char* version_number = "0.1";
+
+// Use operator string to see which operation to perform
+long eval_op(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0) { return x - y; }
+	if (strcmp(op, "*") == 0) { return x * y; }
+	if (strcmp(op, "/") == 0) { return x / y; }
+	return 0;
+}
+
+long eval(mpc_ast_t* t) {
+	// if number, return it directly
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	} else {
+		// the operator is always in the second child
+		char* operator = t->children[1]->contents;
+
+		long x = eval(t->children[2]);
+
+		int i = 3;
+		// use the same operator for the remaining children
+		while (strstr(t->children[i]->tag, "expr")) {
+			x = eval_op(x, operator, eval(t->children[i]));
+			i++;
+		}
+
+		return x;
+	}
+}
 
 int main(int argc, char** argv) {
 
@@ -60,8 +90,8 @@ int main(int argc, char** argv) {
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Mexlisp, &r)) {
 			// success
-			mpc_ast_print(r.output);
-			mpc_ast_delete(r.output);
+      		printf("%li\n", eval(r.output));
+      		mpc_ast_delete(r.output);
 		} else {
 			// error
 			mpc_err_print(r.error);
